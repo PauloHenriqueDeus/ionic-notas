4846be3743e6c03432b9f212a2a7469fcfdd5a0a
Paulo Henrique Alves de Deus <PADEUS@edu.unisinos.br>
2018-11-09 10:57:28 -0200

    Add Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { TabsControllerPage } from '../pages/tabs-controller/tabs-controller';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { DataServiceProvider } from '../providers/data-service/data-service';
+import { AngularFireModule } from '../../node_modules/@angular/fire';
+import { AngularFireDatabaseModule } from '../../node_modules/@angular/fire/database';
+import { AngularFirestoreModule } from '../../node_modules/@angular/fire/firestore';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +26,16 @@ import { DataServiceProvider } from '../providers/data-service/data-service';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyBknCaqinBYkbcyUsYN_8CQWsd77I6gDHU",
+      authDomain: "notas-e9c03.firebaseapp.com",
+      databaseURL: "https://notas-e9c03.firebaseio.com",
+      projectId: "notas-e9c03",
+      storageBucket: "notas-e9c03.appspot.com",
+      messagingSenderId: "507976603435"
+    }, 'notas'), 
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/providers/data-service/data-service.ts
+++ src/providers/data-service/data-service.ts
@@ -1,6 +1,10 @@
 //import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Nota } from './nota';
+import { AngularFireDatabase } from '../../../node_modules/@angular/fire/database';
+//import { Observable } from '../../../node_modules/rxjs-compat';
+import { AngularFirestore } from '../../../node_modules/@angular/fire/firestore';
+import { subscribeOn } from '../../../node_modules/rxjs/operator/subscribeOn';
 
 /*
   Generated class for the DataServiceProvider provider.
@@ -12,10 +16,23 @@ import { Nota } from './nota';
 export class DataServiceProvider {
 
   notas:Nota[] = [];
-
+  
   currentNota:Nota;
-  constructor(/*public http: HttpClient*/) {
-    this.NewNota();   
+  constructor(/*public http: HttpClient, */private db: AngularFireDatabase) {
+   // this.notas.push(new Nota(1));
+     let d:Observable<Nota[]> = db.list<Nota>('items').valueChanges();
+     //db.list('items').push(this.notas);
+     //console.log(d);
+
+     d.subscribe(a =>{
+       
+       for (let index = 0; index < a.length; index++) {
+         const element = a[index][0];
+         this.notas.push(element);
+       }
+     });
+
+     console.log('notas', this.notas);
   }
 
   NewNota(){
